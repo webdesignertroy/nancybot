4f59d8529f50b3d936bdf9dcfe1edc0cf657ab56
webdesignertroy <sava.rowtham@yahoo.com>
2016-05-19 19:59:18 -0700

    change to test

--- scripts/my-first-bot.js
+++ scripts/my-first-bot.js
@@ -1,5 +1,5 @@
 module.exports = funciton(robot) {
 	robot.hear(/These boots/, function(response) {
-		return response.send('.. are made for walkin')
-	}
-}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+		return response.send('.. are made for walkin');
+	});
+}
