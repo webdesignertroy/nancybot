bf98c34865c25f680f26578d3540c1c47d6a3d9d
Natsuko <natsuko_eguchi@yahoo.com>
2016-05-19 22:04:39 -0700

    ne

--- scripts/my-first-bot.js
+++ scripts/my-first-bot.js
@@ -1,6 +1,6 @@
 module.exports = function(robot) {
 	robot.hear(/These boots/, function(response) {
-		return response.send('.. are made for walkin');
+		return response.send('.. are made for walkingg');
 	});
 }
 // hello
