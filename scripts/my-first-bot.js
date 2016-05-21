bf2b36a7e7a78e883f71cde7715859ebaa9de76e
webdesignertroy <sava.rowtham@yahoo.com>
2016-05-20 21:07:16 -0700

    Added module.export

--- scripts/my-first-bot.js
+++ scripts/my-first-bot.js
@@ -1,15 +1,17 @@
-robot.respond(/have a soda/i, function(res) {
-  var sodasHad;
-  sodasHad = robot.brain.get('totalSodas') * 1 || 0;
-  if (sodasHad > 4) {
-    return res.reply("I'm too fizzy..");
-  } else {
-    res.reply('Sure!');
-    return robot.brain.set('totalSodas', sodasHad + 1);
-  }
-});
+ module.exports = function(robot) { 
+  robot.respond(/have a soda/i, function(res) {
+    var sodasHad;
+    sodasHad = robot.brain.get('totalSodas') * 1 || 0;
+    if (sodasHad > 4) {
+      return res.reply("I'm too fizzy..");
+    } else {
+      res.reply('Sure!');
+      return robot.brain.set('totalSodas', sodasHad + 1);
+    }
+  });
 
-robot.respond(/sleep it off/i, function(res) {
-  robot.brain.set('totalSodas', 0);
-  return msg.reply('zzzzz');
-});
+  robot.respond(/sleep it off/i, function(res) {
+    robot.brain.set('totalSodas', 0);
+    return msg.reply('zzzzz');
+  });
+};
