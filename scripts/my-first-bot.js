2b714628e8be3e06559295044fba08986f44876a
webdesignertroy <sava.rowtham@yahoo.com>
2016-05-20 22:04:24 -0700

    Made more revisions

--- scripts/my-first-bot.js
+++ scripts/my-first-bot.js
@@ -1,9 +1,23 @@
  module.exports = function(robot) { 
   robot.respond(/do you want to play/i, function(res) {
     var sodasHad;
+    var sodasRes;
+
+    switch(){
+      case 1:
+        sodasRes = "once";
+      break;
+      case 2:
+        sodasRes = "twice";
+      break;
+      default:
+        sodaRes = sodasHad + " times";
+      break;
+    }
+
     sodasHad = robot.brain.get('totalSodas') * 1 || 0;
-    if (sodasHad > 1) {
-      return res.reply("I already played" + sodasHad + " times with you.");
+    if (sodasHad > 0) {
+      return res.reply("We already played " + sodaRes +".");
     } else {
       res.reply('Sure!');
       return robot.brain.set('totalSodas', sodasHad + 1);
