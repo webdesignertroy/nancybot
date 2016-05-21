1f83debd9a941442663cc0fba09e6812a8ee575a
webdesignertroy <sava.rowtham@yahoo.com>
2016-05-20 20:19:16 -0700

    experiment data save

--- scripts/my-first-bot.js
+++ scripts/my-first-bot.js
@@ -1,6 +1,21 @@
 module.exports = function(robot) {
-	robot.hear(/These boots/, function(response) {
+	/*robot.hear(/These boots/, function(response) {
 		return response.send('.. are made for walkin');
-	});
+	});*/
+robot.respond /have a soda/i, (res) ->
+  # Get number of sodas had (coerced to a number).
+  sodasHad = robot.brain.get('totalSodas') * 1 or 0
+
+  if sodasHad > 2
+    res.reply "I'm too fizzy.."
+
+  else
+    res.reply 'Sure!'
+
+    robot.brain.set 'totalSodas', sodasHad+1
+robot.respond /sleep it off/i, (res) ->
+  robot.brain.set 'totalSodas', 0
+  msg.reply 'zzzzz'
+
 }
 // hello
