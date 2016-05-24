be3822cd8811e254ba09ba562402a1ba93edec68
Natsuko <natsuko_eguchi@yahoo.com>
2016-05-23 17:33:48 -0700

    Natsuko

--- scripts/natsuko-bot.js
+++ scripts/natsuko-bot.js
@@ -75,7 +75,7 @@ bot.hear(/Hello!/, function(res) {
 module.exports = function(robot) {
 
     robot.hear(/Nancy? || special/i, function(greeting) {
-        greeting.send("Hello, I can help you stay healthy. Please enter your weight and height. Please enter the data in this format; Weight: xx Foot: xx Inch: xx.");
+        greeting.send("Hello, I can help you stay healthy. Please enter your weight and height in this format; " + <br /> + "Weight: xx Foot: xx Inch: xx.");
     });
 
     robot.respond(/Weight: (.*)/i, function(weightData) {
