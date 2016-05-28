807b8b566cf700701da96b88d06baf52d3d7b7cf
Natsuko <natsuko_eguchi@yahoo.com>
2016-05-28 09:55:24 -0700

    Natsuko

--- scripts/natsuko-bot.js
+++ scripts/natsuko-bot.js
@@ -94,6 +94,39 @@ module.exports = function(robot) {
         var userInch = parseInt(inchData.match[1]);
 
         return inchData.send(userInch + ' inch');
+
+
+        
+
+    var foot = userFoot;
+    var inch = userInch;
+    var lbs = userWeight;
+
+        var allInches = foot * 12 + inch; 
+
+        //var weight = lbs * .45; // Needed for metric conversion 
+        var weight = lbs * 703;
+
+        //var height = allInches * .025; // Converting to meter and needed for metric conversion
+        var height = allInches;
+
+        var rawBMI = weight / (height * height)
+        var BMI = Math.floor(rawBMI * 10) / 10; // Decimal rounding
+
+        if (BMI <= 18.5) {
+            return response.send('Your BMI is ' + BMI + '. You are considered as underweight.' + foot + inch + lbs);
+        } else if (BMI > 18.5 && BMI < 25) {
+            return response.send('Your BMI is ' + BMI + '. You are considered as normal weight.' +foot + inch + lbs);
+        } else if (BMI > 25 && BMI < 30) {
+            return response.send('Your BMI is ' + BMI + '. You are considered as normal overweight.' + foot + inch + lbs);     
+        } else {
+            return response.send('Your BMI is ' + BMI + '. You are considered as obesity.' +foot + inch + lbs);
+        }
+
+
+
+
+
     })
 
     var foot = userFoot;
