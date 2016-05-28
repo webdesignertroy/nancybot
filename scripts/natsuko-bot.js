7db72814b7949f13285c90e52efb9337f7422f46
Natsuko <natsuko_eguchi@yahoo.com>
2016-05-28 10:11:29 -0700

    Natsuko

--- scripts/natsuko-bot.js
+++ scripts/natsuko-bot.js
@@ -83,17 +83,17 @@ module.exports = function(robot) {
     var userWeight = robot.respond(/Weight: (.*)/i, function(weightData) {
         var userWeight = parseInt(weightData.match[1]);
 
-        return 1;
+        return weightData.send(1);
     })
     var userFoot = robot.respond(/Foot: (.*)/i, function(footData) {
         var userFoot = parseInt(footData.match[1]);
 
-        return 2;
+        return footData.send(2);
     })
     var userInch = robot.respond(/Inch: (.*)/i, function(inchData) {
         var userInch = parseInt(inchData.match[1]);
 
-        return 3;
+        return inchData.send(3);
     })
 
     var foot = userFoot;
